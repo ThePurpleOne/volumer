1189763f677b01b33bb19173b2003508a56aa476
Jonas <jonas.stirnemann@etu.hesge.ch>
2023-10-01 15:29:04 +0200

    Added some stuff to python version

--- src/pc/PYTHON/main.py
+++ src/pc/PYTHON/main.py
@@ -1,68 +1,26 @@
-import tkinter as tk
-from ttkthemes import ThemedTk
-
-def setup_window():
-	window = tk.Tk()
-	window.title("Volume Control")
-	window.geometry("300x300")
-
-	# ! COMBOBOX FOR APPS
-	apps = [app.Process.name() for app in get_open_apps() if app.Process]
-	app_var = tk.StringVar(window)
-	app_var.set(apps[0])
-	app_selector = tk.OptionMenu(window, app_var, *apps)
-	app_selector.pack()
-
-	# ! SLIDER
-	volume_slider = tk.Scale(window, from_=0, to=100, orient=tk.HORIZONTAL)
-	volume_slider.set(50)
-
-	def slider_callback(event):
-		slider_value = volume_slider.get()
-		selected_app = app_var.get()
-		set_app_volume(selected_app, slider_value / 100)
-
-	volume_slider.bind("<ButtonRelease-1>", slider_callback)
-	volume_slider.pack()
-
-	return window
-
-
-
-def get_open_apps():
-	from pycaw.pycaw import AudioUtilities
-	return AudioUtilities.GetAllSessions()
-
-def show_open_apps():
-	for session in get_open_apps():
-		try:
-			volume = session.SimpleAudioVolume
-			if session.Process:
-				print(f"{volume.GetMasterVolume():.02f} | {session.Process.name()}")
-		except Exception as e:
-			print(e)
-
-def set_app_volume(app_name, volume):
-	from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
-
-	sessions = get_open_apps()
-
-	for session in sessions:
-		if session.Process and session.Process.name() == app_name:
-			app = session.SimpleAudioVolume
-			app.SetMasterVolume(volume, None)
-			return
-		
-	raise Exception(f"App {app_name} not found")
-
-
+from ui import setup_window
+from volume_control import show_open_apps, get_installed_apps, list_installed_apps
 
 
 def main():
-	show_open_apps()
-	window = setup_window()
-	window.mainloop()
+	import toml
+	config = toml.load('config.toml')
+
+	#show_open_apps()
+	#window = setup_window()
+	#window.mainloop()
+	#apps = get_installed_apps()
+	#print(*apps, sep="\n")
+
+	#installed_apps = list_installed_apps()
+	#print("List of Installed Apps:")
+	#for app in installed_apps:
+	#	print(app)
+
+	print(config.get("knob2").get("apps"))
 
 
 if __name__ == '__main__':
-	main()
+	#main()
+
+	show_open_apps()
--- src/pc/go.mod
+++
@@ -1,5 +0,0 @@
-module main
-
-go 1.21.0
-
-require github.com/karalabe/hid v1.0.0
